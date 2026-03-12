aea21453d83887cb48cbae7d136e15387ffb713f
liudmila198 <belokopytova.ln@gmail.com>
2026-03-12 20:33:23 +0300

    79

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
+import authStorage from '../services/authStorage'
 
 const instance = axios.create({
   baseURL: '',
 })
 
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
+  const token = authStorage.getToken()
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
